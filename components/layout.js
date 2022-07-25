abcaaebfaeca0f4cb158214228121c67dbb1c024
Aurélien Audelin <aurelien.audelin@gmail.com>
2022-07-25 09:15:19 +0200

    Fix google analytics

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,25 @@
 import Footer from './footer'
 import Meta from './meta'
+import Script from "next/script";
 
-export default function Layout({ preview, children }) {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+export default function Layout({preview, children}) {
+    return (
+        <>
+            <Meta/>
+            <Script async strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-BSJT1Q7WMN"></Script>
+            <Script strategy="lazyOnload" type="text/javascript">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+    
+                    gtag('config', 'G-BSJT1Q7WMN');
+                `}
+            </Script>
+            <div className="min-h-screen">
+                <main>{children}</main>
+            </div>
+            <Footer/>
+        </>
+    )
 }
--- components/meta.js
+++ components/meta.js
@@ -39,16 +39,6 @@ export default function Meta() {
                 content={`Contenful Bot automates workflows between apps using the power of Zapier or Make`}
             />
             <meta property="og:image" content={HOME_OG_IMAGE_URL}/>
-            <Script strategy="lazyOnload" async ßsrc="https://www.googletagmanager.com/gtag/js?id=G-BSJT1Q7WMN"></Script>
-            <Script strategy="lazyOnload">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'G-BSJT1Q7WMN');
-                `}
-            </Script>
         </Head>
     )
 }
