7df2425cc353050fa581e8e4e3d653fbf218b4d9
Aurélien Audelin <aurelien.audelin@gmail.com>
2022-08-01 22:01:22 +0200

    Add google ads

--- components/layout.js
+++ components/layout.js
@@ -16,6 +16,18 @@ export default function Layout({preview, children}) {
                     gtag('config', 'G-BSJT1Q7WMN');
                 `}
             </Script>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <Script async strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=UA-235375056-1">
+            </Script>
+            <Script strategy="lazyOnload" type="text/javascript">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-235375056-1');
+                `}
+            </Script>
             <div className="min-h-screen">
                 <main>{children}</main>
             </div>
